c8a0d7f87d4b3022971ec5c2184753e93016086d
Ahmed Osama <32176872+ahmedosama07@users.noreply.github.com>
2024-03-25 00:45:08 +0200

    Update repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -1,5 +1,11 @@
+#!/bin/bash                                                                     
+                                                                                
+# Directory containing test folders                                             
+testing_folder="test"                                                           
+build_folder="build"
+mkdir $testing_folder
 for i in {1..1000}
 do
-  echo "Test(" $i ") ................................................. :-" >> final_result.out
-  ./caltrain >> final_result.out
+  echo "Test(" $i ") ................................................. :-" >> $testing_folder/final_result.out
+  ./$build_folder/caltrain >> $testing_folder/final_result.out
 done
